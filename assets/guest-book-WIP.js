92001889227fd629d312ea118a75299f6f7e27d6
davidbroadfoot <davidjbfoot@gmail.com>
2024-05-26 08:58:56 -0400

    Update guest-book-WIP.js

--- assets/guest-book-WIP.js
+++ assets/guest-book-WIP.js
@@ -256,7 +256,7 @@ subscribeForm.innerHTML = ` <h1>Sign The Guestbook</h1>
   oninput="this.setCustomValidity('')"  maxlength="50" placeholder="Enter Your Message Here" required></textarea>
       
          <label for="g-recaptcha"  class="aterisk_after">Captcha</label>      
-  <div style="text-align:center"> <div class="g-recaptcha"
+  <div style="text-align:centre"> <div class="g-recaptcha"
        data-sitekey="${CaptchaKey}"></div> </div>
       
         <input class="form-button" type="submit" value="Submit">
